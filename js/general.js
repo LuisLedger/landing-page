fa907a23f12825bb276b37b1928fb5418b2e920a
Luis <rdam_azul@hotmail.com>
2019-06-14 09:23:11 -0700

    Upload test project

---
+++ js/general.js
@@ -0,0 +1,30 @@
+var main = {
+
+    ready: function(){
+
+        var t = this.components;
+        
+        $('.view-portfolio-detail').on('click',function(e){
+            e.preventDefault()
+            
+            t.show_portfolio_items( $(this).attr('data-type') )
+        })
+    },
+
+    components: {
+        /* PORTFOLIO EVENTS*/
+        show_portfolio_items(attribute = 'all'){
+            if (attribute == 'all') {
+                $('.portfolio-item').show()
+            } else {
+                $('.portfolio-item').hide()
+                $('.'+attribute).show()
+            }
+        }
+    }
+
+}
+$(document).ready(function(){
+
+    main.ready();
+})
